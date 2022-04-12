64397d7c695000d1bac8d25631ae7e41782bff62
firefirer1983 <fyman.zhang@gmail.com>
2022-04-12 09:51:54 +0800

    update

---
+++ insert_lock_test.py
@@ -0,0 +1,41 @@
+
+import sqlalchemy as sa
+import pymysql
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from contextlib import contextmanager
+pymysql.install_as_MySQLdb()
+Base = declarative_base()
+
+
+class Box(Base):
+
+    __tablename__ = "tb_box"
+
+    id = sa.Column("id", sa.Integer, primary_key=True)
+    serial_num = sa.Column("sn", sa.String(50), nullable=False)
+
+
+db = sa.create_engine("mysql://xy:123456789@127.0.0.1:3306/test?charset=utf8mb4", echo=True)
+Session = sessionmaker(bind=db)
+ScopedSession = scoped_session(Session)
+
+
+@contextmanager
+def session_scoped():
+    s = Session()
+    try:
+        yield s
+    except Exception as e:
+        s.rollback()
+        raise e
+
+
+
+def main():
+    Base.metadata.create_all(db)
+
+
+
+if __name__ == "__main__":
+    main()
